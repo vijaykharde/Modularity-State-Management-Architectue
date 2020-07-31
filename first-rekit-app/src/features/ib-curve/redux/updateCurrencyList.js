57e97a0452a6b0169955120158fbbf37a389208a
Vijay Kharde <vijay.kharde@gmail.com>
2020-07-31 23:18:03 +0530

    Chapter 3 : Reducers added

--- first-rekit-app/src/features/ib-curve/redux/updateCurrencyList.js
+++ first-rekit-app/src/features/ib-curve/redux/updateCurrencyList.js
@@ -1,3 +1,20 @@
-export function updateCurrencyList() {
+import { IB_CURVE_UPDATE_CURRENCY_LIST } from './constants';
 
+export function updateCurrencyList(currList) {
+    return {
+        type: IB_CURVE_UPDATE_CURRENCY_LIST,
+        currList: currList
+    };
+}
+
+export function reducer(state, action) {
+    switch (action.type) {
+        case IB_CURVE_UPDATE_CURRENCY_LIST:
+            return {
+                ...state,
+                currList: action.currList
+            };
+        default:
+            return state;
+    }
 }
